4e447b6b2bd9c6113c66350259c9252cb2b635ae
HliasK <iliaskostapappas13@gmail.com>
2026-07-10 14:04:18 +0300

    Fix loader

--- horoscope-loader.js
+++ horoscope-loader.js
@@ -1,6 +1,7 @@
 async function loadHoroscope(sign) {
     try {
         const response = await fetch("./horoscopes.json?v=" + Date.now());
+
         const data = await response.json();
 
         const container = document.getElementById("daily-horoscope");
@@ -9,10 +10,10 @@ async function loadHoroscope(sign) {
 
         container.innerHTML = `
             <h2>🔮 Ημερήσια Πρόβλεψη ${sign}</h2>
-            <p>${data.signs[sign] || "Η πρόβλεψη δεν είναι διαθέσιμη."}</p>
+            <p>${data.signs[sign].replace(/\n/g, "<br>")}</p>
         `;
 
     } catch (err) {
-        console.error(err);
+        console.error("Horoscope error:", err);
     }
 }
